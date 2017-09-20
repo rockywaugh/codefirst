4d9126ed4a785b072d0daf9497c7a4c3a3851dea
Raquache Waugh <raquache.waugh@aero.org>
2017-09-20 15:31:45 -0700

    books mongo file and update to app

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI', {
 	useMongoClient: true
@@ -10,9 +11,21 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
+	.post(function(req, res) {
+		var book = new Book(req.body);
+
+		// console.log(book);
+
+		book.save();
+		res.status(201).send(book); // Status 201 - created
+	})
+
 	.get(function(req, res) {
 		Book.find(function(err, books) {
 			if (err) {
